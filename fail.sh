d918b9542fdb415d79dc9c0daea6f11ac1f26f47
Valentin Hilbig <webmaster@scylla-charybdis.com>
2016-07-12 10:47:37 +0200

    fail.sh - hook to resolve problems semi-automatically if possible

---
+++ fail.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# This is called when a failure is detected (connection problem)
+# Most often this means, the remote machine has crashed or a network outage
+#
+# Try to resolve things automatically, if you are not able to do so, drop in a shell
+
+# Check if machine available, then just retry
+ssh -o BatchMode=yes -o KbdInteractiveAuthentication=no -o PasswordAuthentication=no -o StrictHostKeyChecking=no -o ConnectTimeout=10 -o ServerAliveInterval=1 -o ServerAliveCountMax=3 "$HOST" true && exit
+
+# Reset the remote mache with some obsuce procedure
+robot/rescue.sh && exit
+
+# Nope, enter shell, leave it to the admin
+echo
+date
+exec bash
